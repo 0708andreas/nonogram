9fe6f81b78362f12e9fe432740dfa091b8e33f84
andreas <andreas@lha66.dk>
2023-08-31 15:39:47 +0200

    Omstruktureret, så beskrivelsen af nonogrammet nu er i en input-fil

--- generator.py
+++ generator.py
@@ -23,73 +23,120 @@ XXXXXXXXXXXXXX------
 -----X----X---------
 -----XX---XX--------""".split("\n")[1:]
 
-# Euler-formel
+# Euler-formel, løsbar iflg https://nonogram.kniffen.dev/
 m = """
--XX-XX------
--X-X-X------
--XX-XX---X--
---------XX--
--XXXXXX--X--
--X-------X--
---X-----XXX-
----X--------
---X----XXXXX
--X----------
--XXXXXX--X-X
------------X
-X----XXX-X-X
---XX-X-X-X--
-X----X-X-X-X
-X-XX-X-X----
-X----XXX----""".split("\n")[1:]
+--XX-XX-------------
+--X-X-X--------X----
+--XX-XX-------XX----
+---------------X----
+-XXXXXXXX------X----
+--XX----X------X----
+---XX---------XXX---
+----XX--------------
+---XX------XXXXXXXXX
+--XX----X-----------
+-XXXXXXXX----X---X--
+X------------X---X--
+X------XXX---X-X-X--
+X-X-XX-X-X---XX--X--
+XX-----X-X---X-X----
+X-X-XX-X-X---X-X-X--
+X-X----XXX----------""".split("\n")[1:]
 
 # Mario bullet
-m = """
----XXXX--XXXXXXX-----------
---XXXXXXXXXXXXXXXXXX-------
--X---XXXX-----X--XXXXX-----
--X---X-XX-----X---XXXXX----
-XXXXX-XXXXXXXXXX--XXXXXX---
-XXXXX-X-XXXXXXXXXXX--XXXX--
-XXXXX-X-XXXXXXXXXX---X-XXX-
-XXXXX-X-XXXXXXXXXX--X-X-XX-
-XXXXX-X-XXXXXXXX-XX--XXXXXX
-XXXXX-X-XXXXXXX---XX---XXXX
-XXXXX-X-XXXXXX-X--XXXXXXXXX
-XXXXX-X-XXXXXX-X---XXXXXXXX
-XXXXX-X-XXXXXX-X----XXXXXXX
-XXXXX-X-XXXXXX--XXX---XXXXX
-XXXXX-X-XXXXXX--XXX-----XX-
--XXXX-X-XXXXXXX-XXXXXX--X--
--XXXXX-X-XXXXXX---XXXXXX---
---XXXX-XXXXXXXXX---XXX-----
---XXXXX--XXXXXXXXXXX-------""".split("\n")[1:]
+# m = """
+# ---XXXX--XXXXXXX-----------
+# --XXXXXXXXXXXXXXXXXX-------
+# -X---XXXX-----X--XXXXX-----
+# -X---X-XX-----X---XXXXX----
+# XXXXX-XXXXXXXXXX--XXXXXX---
+# XXXXX-X-XXXXXXXXXXX--XXXX--
+# XXXXX-X-XXXXXXXXXX---X-XXX-
+# XXXXX-X-XXXXXXXXXX--X-X-XX-
+# XXXXX-X-XXXXXXXX-XX--XXXXXX
+# XXXXX-X-XXXXXXX---XX---XXXX
+# XXXXX-X-XXXXXX-X--XXXXXXXXX
+# XXXXX-X-XXXXXX-X---XXXXXXXX
+# XXXXX-X-XXXXXX-X----XXXXXXX
+# XXXXX-X-XXXXXX--XXX---XXXXX
+# XXXXX-X-XXXXXX--XXX-----XX-
+# -XXXX-X-XXXXXXX-XXXXXX--X--
+# -XXXXX-X-XXXXXX---XXXXXX---
+# --XXXX-XXXXXXXXX---XXX-----
+# --XXXXX--XXXXXXXXXXX-------""".split("\n")[1:]
 
+# # Kanin
+# m = """
+# ------XXXXXXXXXX-----------
+# ------XX-----XXXX----------
+# ------XXXXXXX-XXXX---------
+# -----XXXXXXXXX-XXXXXXX-----
+# ----XXX-----XXX-XXXXXXXX---
+# ----XXX-------XXXXXXXXXXX--
+# -----XXX-------XXXXXXXXXXX-
+# -----XXXXXXXXXX-XXXX--XXXX-
+# ------XXXXXXXXXXXXXX--XXXXX
+# --------XXXXX-XXXXXXXXXXXXX
+# -------------XXXXXXXXXXXXXX
+# --------------XXXXXXXXXXXX-
+# ---------XXXXXXXXXXXXXXXX--
+# -------XXXXXXXX-XXXXXXXX---
+# -----XXXXXXXXXXX-XXXXXX----
+# ----XXXXXXXXXXXXXX---XXX---
+# ----XXXXXXXXXXXXXXXXXXXX---
+# ---XXXXXXXXXXXXXXXXXXXXX---
+# ---XXXXXXXXXXXXXXXXXXXXX---
+# --XXXXXXXXX--XXXXXXXXXXX---
+# --XXXXXXXXXXX-XXXXXXXXXX---
+# XX-XXXXXXXXXX-XXXXXXXXX----
+# X-XXXXXXXXXXXX-XXXX-XX-----
+# X-XXXXXXXXXXXX-XXX-XXX-----
+# XX-XXXXXXXXXX-XXXXXX-XX----""".split("\n")[1:]
 solution = False
 
 # print(m)
+#
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog = 'Nonogram tex-file generator',
+    description = 'Generates a TEX-file for a nonogram puzzle.')
+
+parser.add_argument('infile')
+parser.add_argument('outfile')
+parser.add_argument('-s', '--solution', action='store_true')
+
+args = parser.parse_args()
+infile, outfile, solution = (args.infile, args.outfile, args.solution)
+
+with open(infile, 'r') as f:
+    m = f.read().split("\n")[:-1]
+    print(m)
 
 rows = [[(next(group)[0], 1+sum(1 for _ in group)) for k, group in groupby(enumerate(line), key=lambda x: x[1]) if k == "X"]
                                for line in m]
 cols = [[(next(group)[0], 1+sum(1 for _ in group)) for k, group in groupby(enumerate(line), key=lambda x: x[1]) if k == "X"]
                                for line in zip(*m)]
-# print(*rows, sep="\n")
-# print()
-# print(*cols, sep="\n")
 
-print("""\\documentclass[tikz]{standalone}
+output = ""
+output += """\\documentclass[tikz]{standalone}
 \\usepackage{logicpuzzle}
 
 \\begin{document}
 
-""")
+"""
 
-print(f"\\begin{{center}}\\begin{{nonogram}}[rows={len(rows)}, columns={len(cols)}, fontsize=huge]")
-print("\\nonogramV{" + ", ".join("{" + ", ".join(map(lambda x: str(x[1]), ix[::-1])) + "}" for ix in rows[::-1]) + "}")
-print("\\nonogramH{" + ", ".join("{" + ", ".join(map(lambda x: str(x[1]), ix[::-1])) + "}" for ix in cols) + "}")
+output += f"\\begin{{center}}\\begin{{nonogram}}[rows={len(rows)}, columns={len(cols)}, fontsize=huge]"
+output += "\\nonogramV{" + ", ".join("{" + (", ".join(map(lambda x: str(x[1]), ix[::-1])) if ix else "0") + "}" for ix in rows[::-1]) + "}"
+output += "\\nonogramH{" + ", ".join("{" + (", ".join(map(lambda x: str(x[1]), ix[::-1])) if ix else "0") + "}" for ix in cols) + "}"
 if solution:
     for i, row in enumerate(rows[::-1]):
-        print("\\nonogramrow{", i+1, "}{", ", ".join([f"{i+1}/{l}" for i, l in row]) + "}")
-print("\\end{nonogram}")
-print("\\end{center}")
-print("\\end{document}")
+        if row:
+            output += f"\\nonogramrow{{ {i+1} }}{{" + ", ".join([f"{i+1}/{l}" for i, l in row]) + "}"
+output += "\\end{nonogram}"
+output +="\\end{center}"
+output += "\\end{document}"
+
+with open(outfile, 'w') as f:
+    f.write(output)
